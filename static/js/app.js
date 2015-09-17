17091b0c61dd0a21b5ecf1c6fb26bd24786e5cf7
Ned Jackson Lovely <njl@njl.us>
2015-09-17 13:58:09 -0400

    reason only on nay votes

--- static/js/app.js
+++ static/js/app.js
@@ -8,7 +8,18 @@ function reason_length_counter(ev){
     character_count.text(63-content.length);
 }
 
+function vote_radio_click(ev){
+    var v = $('.vote-radio input:checked').val();
+    if(v == 'nay'){
+        $('#reasons-panel').show()
+    }else{
+        $('#reasons-panel').hide()
+        $('#reason').val('').change();
+    }
+}
+
 $(document).ready(function(){
     $('#comment-reason').on('change', vote_comment_reason_handler);
     $('#reason').on('keyup change', reason_length_counter);
+    $('.vote-radio').on('click', vote_radio_click);
 });
