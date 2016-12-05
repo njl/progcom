83e5227273ba690ae3477d1d6d61f13a3da59a46
Ned Jackson Lovely <njl@njl.us>
2016-12-05 12:17:03 -0500

    Poking at the pycon api issues

--- pull_updates.py
+++ pull_updates.py
@@ -45,7 +45,7 @@ TALK_IDS_FORCE = [1553, 1554, 1555, 1556, 1557, 1559, 1560, 1561, 1562, 1565,
 TALK_IDS_FORCE = []
 
 def fetch_ids():
-    raw = api_call('/2017/pycon_api/proposals/?type=talk')
+    raw = api_call('/2017/pycon_api/proposals/?type=talk&limit=5000&status=undecided')
     #print len(raw['data'])
     rv = [x['id'] for x in raw['data']]
     return list(set(TALK_IDS_FORCE + rv + l.get_all_proposal_ids()))
